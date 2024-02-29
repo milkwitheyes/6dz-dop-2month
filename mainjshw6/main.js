7abf0084709ade073dd67112309f7e8b22c990d5
milkwitheyes <154975761+milkwitheyes@users.noreply.github.com>
2024-02-29 15:00:19 +0600

    Add files via upload

---
+++ mainjshw6/main.js
@@ -0,0 +1,8 @@
+document.getElementById('openModal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'block';
+  });
+  
+  document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+  });
+  
